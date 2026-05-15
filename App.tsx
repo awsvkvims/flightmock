d724705f0b3a2bca4fb07627ea44e0b11f7530c1
Vimlesh Kumar <vimlesh@vimsco-consulting.com>
2026-05-15 00:33:46 -0400

    feat: add Maestro E2E setup with smoke test passing

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>✈️ FlightMock ✈️</Text>
+      <Text>FlightMock ✈️</Text>
       <StatusBar style="auto" />
     </View>
   );
